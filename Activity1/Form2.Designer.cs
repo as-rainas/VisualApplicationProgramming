02807d0c7d61f914bde73b9467c4157be6683509
Rainas <rainasmohammed@gmail.com>
2023-05-23 04:20:44 +0530

    activity1 - done

---
+++ Activity1/Form2.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Activity1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNumber1 = new TextBox();
+            txtNumber2 = new TextBox();
+            txtResult = new TextBox();
+            btnSum = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(80, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Number 1 *";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(80, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Number 2 *";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(80, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Result";
+            // 
+            // txtNumber1
+            // 
+            txtNumber1.Location = new Point(154, 57);
+            txtNumber1.Name = "txtNumber1";
+            txtNumber1.Size = new Size(184, 23);
+            txtNumber1.TabIndex = 3;
+            txtNumber1.Text = "0";
+            txtNumber1.Validating += txtNumber1_Validating;
+            // 
+            // txtNumber2
+            // 
+            txtNumber2.Location = new Point(154, 107);
+            txtNumber2.Name = "txtNumber2";
+            txtNumber2.Size = new Size(184, 23);
+            txtNumber2.TabIndex = 4;
+            txtNumber2.Text = "0";
+            txtNumber2.Validating += txtNumber2_Validating;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(154, 154);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(184, 23);
+            txtResult.TabIndex = 5;
+            txtResult.Text = "0";
+            // 
+            // btnSum
+            // 
+            btnSum.Location = new Point(154, 197);
+            btnSum.Name = "btnSum";
+            btnSum.Size = new Size(184, 43);
+            btnSum.TabIndex = 6;
+            btnSum.Text = "SUM";
+            btnSum.UseVisualStyleBackColor = false;
+            btnSum.Click += btnSum_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(434, 291);
+            Controls.Add(btnSum);
+            Controls.Add(txtResult);
+            Controls.Add(txtNumber2);
+            Controls.Add(txtNumber1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Aggregate Function - Sum";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNumber1;
+        private TextBox txtNumber2;
+        private TextBox txtResult;
+        private Button btnSum;
+    }
+}
---
+++ Activity1/Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity1
+{
+    public partial class Form2 : Form
+    {
+        private int number1;
+        private int number2;
+        private int result;
+
+        private int inValidation;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            number1 = 0;
+            number2 = 0;
+
+            txtResult.ReadOnly = true;
+        }
+
+        private void txtNumber1_Validating(object sender, CancelEventArgs e)
+        {
+            ErrorProvider errorProvider = new ErrorProvider();
+            errorProvider.SetError(txtNumber1, "");
+
+            int i;
+
+            if (!int.TryParse(txtNumber1.Text, out i))
+            {
+                errorProvider.SetError(txtNumber1, "Please fill the required fields, only numbers are accepted here");
+            }
+        }
+
+        private void txtNumber2_Validating(object sender, CancelEventArgs e)
+        {
+            ErrorProvider errorProvider = new ErrorProvider();
+            errorProvider.SetError(txtNumber2, "");
+
+            int i;
+
+            if (!int.TryParse(txtNumber2.Text, out i))
+            {
+                errorProvider.SetError(txtNumber2, "Please fill the required fields, only numbers are accepted here");
+            }
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ValidateChildren(ValidationConstraints.Enabled)) { 
+                    number1 = int.Parse(txtNumber1.Text);
+                    number2 = int.Parse(txtNumber2.Text);
+
+                    result = (number1 + number2);
+
+                    txtResult.Text = result.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
