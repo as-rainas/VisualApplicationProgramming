25746b7d1368fe0ad7ba319cd178fd3a415dab16
Rainas <rainasmohammed@gmail.com>
2023-05-23 05:30:54 +0530

    Activity 2 done

---
+++ Activity2/Aggregate Functions - Average.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Activity2
+{
+    partial class Aggregate_Functions_Average
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtSubject1 = new TextBox();
+            txtSubject2 = new TextBox();
+            txtAverage = new TextBox();
+            btnSubmit = new Button();
+            lblMark = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Subject 1 *";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(50, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Subject 2 *";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(50, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Average";
+            // 
+            // txtSubject1
+            // 
+            txtSubject1.Location = new Point(125, 36);
+            txtSubject1.Name = "txtSubject1";
+            txtSubject1.Size = new Size(175, 23);
+            txtSubject1.TabIndex = 3;
+            txtSubject1.Text = "0";
+            txtSubject1.Validating += txtSubject1_Validating;
+            // 
+            // txtSubject2
+            // 
+            txtSubject2.Location = new Point(125, 67);
+            txtSubject2.Name = "txtSubject2";
+            txtSubject2.Size = new Size(175, 23);
+            txtSubject2.TabIndex = 4;
+            txtSubject2.Text = "0";
+            txtSubject2.Validating += txtSubject2_Validating;
+            // 
+            // txtAverage
+            // 
+            txtAverage.Location = new Point(125, 100);
+            txtAverage.Name = "txtAverage";
+            txtAverage.Size = new Size(175, 23);
+            txtAverage.TabIndex = 5;
+            txtAverage.Text = "0";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(134, 143);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(147, 40);
+            btnSubmit.TabIndex = 6;
+            btnSubmit.Text = "SUBMIT";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // lblMark
+            // 
+            lblMark.AutoSize = true;
+            lblMark.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            lblMark.ForeColor = Color.Red;
+            lblMark.Location = new Point(287, 156);
+            lblMark.Name = "lblMark";
+            lblMark.Size = new Size(0, 15);
+            lblMark.TabIndex = 7;
+            // 
+            // Aggregate_Functions_Average
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(386, 210);
+            Controls.Add(lblMark);
+            Controls.Add(btnSubmit);
+            Controls.Add(txtAverage);
+            Controls.Add(txtSubject2);
+            Controls.Add(txtSubject1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Aggregate_Functions_Average";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Aggregate Functions -Average";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtSubject1;
+        private TextBox txtSubject2;
+        private TextBox txtAverage;
+        private Button btnSubmit;
+        private Label lblMark;
+    }
+}
---
+++ Activity2/Aggregate Functions - Average.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity2
+{
+    public partial class Aggregate_Functions_Average : Form
+    {
+        private int subject1;
+        private int subject2;
+        private int result;
+
+        private bool bSubjec1 = false;
+        private bool bSubjec2 = false;
+        private bool bResult = false;
+
+        private int average;
+
+        private int intValidation;
+        private ErrorProvider errorProvider = new ErrorProvider();
+
+        public Aggregate_Functions_Average()
+        {
+            InitializeComponent();
+
+            subject1 = 0;
+            subject2 = 0;
+
+            txtAverage.ReadOnly = true;
+
+
+        }
+
+        private void txtSubject1_Validating(object sender, CancelEventArgs e)
+        {
+            errorProvider.SetError(txtSubject1, "");
+
+            if (!int.TryParse(txtSubject1.Text, out intValidation))
+            {
+                bSubjec1 = true;
+                errorProvider.SetError(txtSubject1, "Please fill the required fields, only numbers accepted here");
+            }
+
+        }
+
+        private void txtSubject2_Validating(object sender, CancelEventArgs e)
+        {
+            errorProvider.SetError(txtSubject2, "");
+
+            if (!int.TryParse(txtSubject2.Text, out intValidation))
+            {
+                bSubjec2 = true;
+                errorProvider.SetError(txtSubject2, "Please fill the required fields, only numbers accepted here");
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bSubjec2 == false && bSubjec2 == false)
+                {
+                    subject1 = int.Parse(txtSubject1.Text);
+                    subject2 = int.Parse(txtSubject2.Text);
+
+                    result = ((subject1 + subject2) / 2);
+
+                    txtAverage.Text = result.ToString();
+
+                    if (result >= 0 && result <= 40)
+                    {
+                        lblMark.Text = "Grade F";
+                    }
+                    else if (result >= 40 && result <= 65)
+                    {
+                        lblMark.Text = "Grade C";
+                    }
+                    else if (result >= 65 && result <= 75)
+                    {
+                        lblMark.Text = "Grade B";
+                    }
+                    else if (result >= 75 && result <= 100)
+                    {
+                        lblMark.Text = "Grade A";
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
